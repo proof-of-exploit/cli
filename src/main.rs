2d77f42176a60fc3e92a415e01d5129ab8f0e750
Soham Zemse <22412996+zemse@users.noreply.github.com>
2023-03-14 22:22:12 +0530

    Add basic code in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use zk_proof_of_evm_exploit::{client, env::Env};
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let env = Env::load();
+    let cli = client::setup(env.eth_rpc_url, env.fork_block_number).await;
+    let bn = cli.block_number().unwrap().as_u64();
+    println!("block {}", bn);
 }
