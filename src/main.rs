b52a5f7864d1342e5aa7cc8303b9357dcd905923
Soham Zemse <22412996+zemse@users.noreply.github.com>
2023-03-15 19:23:50 +0530

    Play around with MockProver

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use halo2_proofs::{dev::MockProver, halo2curves::bn256::Fr};
 use zk_proof_of_evm_exploit::BuilderClient;
+use zkevm_circuits::super_circuit::SuperCircuit;
 
 #[tokio::main]
 async fn main() {
@@ -6,19 +8,41 @@ async fn main() {
 
     let builder = BuilderClient::default().await.unwrap();
 
-    builder
+    let chain_id = builder.anvil.eth_chain_id().unwrap().unwrap();
+    let block_number = builder.anvil.block_number().unwrap();
+    println!("chain_id: {:?}, block_number: {:?}", chain_id, block_number);
+
+    let raw_tx = "0x01f88d83aa36a70184ee6b28008301388094df03add8bc8046df3b74a538c57c130cefb89b8680a46057361d0000000000000000000000000000000000000000000000000000000000000001c080a0c555cc61abbe8af27dc54279c25f70d6883841de257f13c56a770b8d07e7d065a06dbaf6fafad5e1735ad3f47af1210b742df31e35c68b8b9eedabdd9e6234291a";
+    let hash = builder
         .anvil
-        .fund_wallet(
-            "0x2CA4c197AE776f675A114FBCB0B03Be845f0316d"
-                .parse()
-                .unwrap(),
-        )
+        .send_raw_transaction(raw_tx.parse().unwrap())
+        .await
+        .unwrap();
+    builder.anvil.mine_one().await;
+
+    let tx = builder
+        .anvil
+        .transaction_by_hash(hash)
+        .await
+        .unwrap()
+        .unwrap();
+
+    println!("tx: {:?}", tx);
+
+    let (circuit_input_builder, _) = builder
+        .gen_inputs(tx.block_number.unwrap().as_usize())
         .await
         .unwrap();
 
-    let (circuit_input_builder, block) = builder
-        .gen_inputs(builder.anvil.block_number().unwrap())
-        .await
-        .unwrap();
-    // println!("block {}", bn);
+    println!("inputs generated");
+
+    let (degree, circuit, instance) =
+        SuperCircuit::<Fr, 1, 256, 0x100>::build_from_circuit_input_builder(&circuit_input_builder)
+            .unwrap();
+
+    println!("proving");
+    let prover = MockProver::run(degree, &circuit, instance).unwrap();
+    println!("proving done, now verifying");
+    let _res = prover.verify_par();
+    println!("verifying done");
 }
