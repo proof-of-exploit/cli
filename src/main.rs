1fc7a296e959f4914c28732edcdd81fab0c39983
Soham Zemse <22412996+zemse@users.noreply.github.com>
2023-03-18 21:36:11 +0530

    Add wait for tx in main script

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,14 @@ async fn main() {
     let block_number = builder.anvil.block_number().unwrap();
     println!("chain_id: {:?}, block_number: {:?}", chain_id, block_number);
 
-    let raw_tx = "0x01f88d83aa36a70184ee6b28008301388094df03add8bc8046df3b74a538c57c130cefb89b8680a46057361d0000000000000000000000000000000000000000000000000000000000000001c080a0c555cc61abbe8af27dc54279c25f70d6883841de257f13c56a770b8d07e7d065a06dbaf6fafad5e1735ad3f47af1210b742df31e35c68b8b9eedabdd9e6234291a";
+    let raw_tx = "0xf88c8084ee6b28008301388094df03add8bc8046df3b74a538c57c130cefb89b8680a46057361d00000000000000000000000000000000000000000000000000000000000000018401546d72a0f5b7e54553deeb044429b394595581501209a627beef020e764426aa0955e93aa00927cb7de78c15d2715de9a5cbde171c7202755864656cd4726ac43c76a9000a";
     let hash = builder
         .anvil
         .send_raw_transaction(raw_tx.parse().unwrap())
         .await
         .unwrap();
-    builder.anvil.mine_one().await;
+
+    builder.anvil.wait_for_transaction(hash).await.unwrap();
 
     let tx = builder
         .anvil
@@ -26,18 +27,16 @@ async fn main() {
         .unwrap()
         .unwrap();
 
-    println!("tx: {:?}", tx);
-
-    println!("inputs generated");
-
     let witness = builder
         .gen_witness(tx.block_number.unwrap().as_usize())
         .await
         .unwrap();
+    println!("witness {:#?}", witness);
     let circuit = ExploitCircuit::<Fr, 1, 256, 0x100>::new_from_block(&witness);
     let (_, rows_needed) = ExploitCircuit::<Fr, 1, 256, 0x100>::min_num_rows_block(&witness);
     let k = log2_ceil(64 + rows_needed);
     let instance = circuit.instance();
+    println!("instance {:#?}", instance);
 
     println!("proving");
     let prover = MockProver::run(k, &circuit, instance).unwrap();
