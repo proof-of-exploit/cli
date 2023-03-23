2f11d6df572f15a9e4f1c007d07d613c544c8a2b
Soham Zemse <22412996+zemse@users.noreply.github.com>
2023-03-24 01:43:13 +0530

    Verification pass

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
 use halo2_proofs::{dev::MockProver, halo2curves::bn256::Fr};
-use zk_proof_of_evm_exploit::{BuilderClient, ExploitCircuit};
-use zkevm_circuits::util::{log2_ceil, SubCircuit};
+
+use zk_proof_of_evm_exploit::{BuilderClient, CircuitsParams};
+use zkevm_circuits::{
+    super_circuit::SuperCircuit,
+    util::{log2_ceil, SubCircuit},
+};
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
 
-    let builder = BuilderClient::default().await.unwrap();
+    const MAX_TXS: usize = 1;
+    const MAX_CALLDATA: usize = 256;
+    const MOCK_RANDOMNESS: u64 = 0x100;
+
+    let builder = BuilderClient::from_circuits_params(CircuitsParams {
+        max_rws: 357,
+        max_txs: MAX_TXS,
+        max_calldata: MAX_CALLDATA,
+        max_copy_rows: 1000,
+        max_exp_steps: 1000,
+        max_bytecode: 512,
+        max_evm_rows: 1000,
+        max_keccak_rows: 1000,
+    })
+    .await
+    .unwrap();
 
     let chain_id = builder.anvil.eth_chain_id().unwrap().unwrap();
     let block_number = builder.anvil.block_number().unwrap();
@@ -20,6 +39,8 @@ async fn main() {
 
     builder.anvil.wait_for_transaction(hash).await.unwrap();
 
+    println!("tx confimed locally");
+
     let tx = builder
         .anvil
         .transaction_by_hash(hash)
@@ -27,13 +48,16 @@ async fn main() {
         .unwrap()
         .unwrap();
 
-    let witness = builder
+    let mut witness = builder
         .gen_witness(tx.block_number.unwrap().as_usize())
         .await
         .unwrap();
+    witness.randomness = Fr::from(0x100);
     println!("witness {:#?}", witness);
-    let circuit = ExploitCircuit::<Fr, 1, 256, 0x100>::new_from_block(&witness);
-    let (_, rows_needed) = ExploitCircuit::<Fr, 1, 256, 0x100>::min_num_rows_block(&witness);
+    let (_, rows_needed) =
+        SuperCircuit::<Fr, MAX_TXS, MAX_CALLDATA, MOCK_RANDOMNESS>::min_num_rows_block(&witness);
+    let circuit =
+        SuperCircuit::<Fr, MAX_TXS, MAX_CALLDATA, MOCK_RANDOMNESS>::new_from_block(&witness);
     let k = log2_ceil(64 + rows_needed);
     let instance = circuit.instance();
     println!("instance {:#?}", instance);
