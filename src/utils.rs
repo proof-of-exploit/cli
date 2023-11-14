23671c8f1f3b83afaa347ca239fd0835e60aa4c8
Soham Zemse <22412996+zemse@users.noreply.github.com>
2023-11-14 20:00:38 +0300

    Check huffc output error

--- src/utils.rs
+++ src/utils.rs
@@ -68,6 +68,14 @@ pub fn compile_huff(source_path_string: String) -> Bytes {
     cmd.arg(source_path_string);
     cmd.arg("-r");
     let output = cmd.output().unwrap();
+    if !output.stderr.is_empty() {
+        println!(
+            "huffc error: {:?}",
+            String::from_utf8(output.stderr).unwrap()
+        );
+        process::exit(1);
+    }
+
     let stdout = String::from_utf8(output.stdout).unwrap();
     Bytes::from_str(stdout.as_str()).unwrap()
 }
